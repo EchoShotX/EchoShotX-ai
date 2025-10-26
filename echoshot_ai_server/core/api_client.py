c92479380bf11d965cb63ce23383718b5415aec5
hamlsy <dltmddud1122@naver.com>
2025-10-26 14:20:18 +0900

    remove: delete .env #3

--- echoshot_ai_server/core/api_client.py
+++ echoshot_ai_server/core/api_client.py
@@ -9,7 +9,6 @@ class SpringAPIClient:
     """Spring API 클라이언트"""
 
     def __init__(self):
-        # Settings에서 값 가져오기
         self.base_url = settings.SPRING_API_BASE_URL.rstrip("/")
         self.timeout = settings.SPRING_API_TIMEOUT
         self.session = self._create_session()
